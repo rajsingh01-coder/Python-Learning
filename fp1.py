f14c16a2b6267e9b8726c477449cd877487aa2de
Raj Singh <rajsingh555554@gmail.com>
2025-11-23 15:50:32 +0530

    added

---
+++ fp1.py
@@ -0,0 +1,7 @@
+#Write a function get_max(num1, num2) that returns the maximum of the two numbers.
+def get_max(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2 
+print(get_max(30, 50))
